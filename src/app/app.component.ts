9488d7b5b13afff44ce9940b7bb412ee09429c32
Mateus De Paula <mateus.dpaula10@gmail.com>
2024-11-25 17:02:41 -0300

    Inicia estrutura

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ComponenteComponent } from './components/componente/componente.component'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ComponenteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
